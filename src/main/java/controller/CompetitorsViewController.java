8549ac45b06e03cfd1fbf4bc346942c446967109
Marek Fudaliński <fudalinm@gmail.com>
2019-08-15 13:51:49 +0200

    Some fixes and adjustments + connection with methods add

--- src/main/java/controller/CompetitorsViewController.java
+++ src/main/java/controller/CompetitorsViewController.java
@@ -1,4 +1,35 @@
 package controller;
 
-public class CompetitorsViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CompetitorsViewController implements Initializable {
+
+
+    @FXML
+    public void goBack(){
+
+    }
+
+    @FXML
+    public void addNewCompetitor(){
+
+    }
+
+    @FXML
+    public void startCompetition(){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+        /** Right click to columns described here
+         * https://stackoverflow.com/questions/26563390/detect-doubleclick-on-row-of-tableview-javafx
+         *  */
+    }
 }
--- src/main/java/controller/MenuBarController.java
+++ src/main/java/controller/MenuBarController.java
@@ -1,4 +1,42 @@
 package controller;
 
-public class MenuBarController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuBarController implements Initializable {
+
+    @FXML
+    public void save(){
+
+    }
+    @FXML
+    public void saveAs(){
+
+    }
+    @FXML
+    public void undo(){
+
+    }
+    @FXML
+    public void redo(){
+
+    }
+    @FXML
+    public void exportResults(){
+
+    }
+    @FXML
+    public void adminModeOn(){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
 }
