957c7d51ad83c9d03b49093cd102621cd5e96c2e
Marek Fudaliński <fudalinm@gmail.com>
2019-08-18 14:45:37 +0200

    adding views and controllers...

---
+++ src/main/java/controller/AddCompetitorController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class AddCompetitorController {
+
+
+    public void cancelAddNewCompetitor(){
+        System.out.format("cancelAddNewCompetitor (TODO:implement me)\n");
+    }
+    public void addNewCompetitor(){
+        System.out.format("addNewCompetitor (TODO:implement me)\n");
+    }
+}
