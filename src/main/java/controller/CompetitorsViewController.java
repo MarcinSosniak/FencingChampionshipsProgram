35913160670358d7597324832b85e8fbe81dbf34
Marek Fudaliński <fudalinm@gmail.com>
2019-08-18 14:33:28 +0200

    Add new competitor finished

--- src/main/java/controller/CompetitorsViewController.java
+++ src/main/java/controller/CompetitorsViewController.java
@@ -2,8 +2,12 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,17 +16,19 @@ public class CompetitorsViewController implements Initializable {
 
     @FXML
     public void goBack(){
-
+        System.out.format("goBack (TODO:implement me)\n");
     }
 
     @FXML
     public void addNewCompetitor(){
-
+        System.out.format("addNewCompetitor (implemented)\n");
+        Stage childScene = ApplicationController.getApplicationController().renderStageAndSetOwner("/addCompetitor.fxml","Dodaj nowego zawodnika",true);
+        childScene.showAndWait();
     }
 
     @FXML
     public void startCompetition(){
-
+        System.out.format("startCompetiton (TODO:implement me)\n");
     }
 
     @Override
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public void selectCompetition(){
+        System.out.format("selectCompetition (TODO:implement me)\n");
+    }
+
+    public void goNext(){
+        System.out.format("goNext (TODO:implement me)\n");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
 }
